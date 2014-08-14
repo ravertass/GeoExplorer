738d2fe87a9ec050691c7dd0fd94fe5c251df891
sfabian <me@sfabian.net>
2014-08-14 16:55:59 +0200

    Started working on the layouts of the activities

---
+++ src/net/sfabian/geoexplorer/ExploreMenuActivity.java
@@ -0,0 +1,15 @@
+package net.sfabian.geoexplorer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ExploreMenuActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_explore_menu);
+	}
+}
--- src/net/sfabian/geoexplorer/MainActivity.java
+++ src/net/sfabian/geoexplorer/MainActivity.java
@@ -1,9 +1,11 @@
 package net.sfabian.geoexplorer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +13,14 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+	
+	/** 
+	 * Called when the Game button is clicked in the main menu
+	 */
+	public void gotoExploreMenu(View view) {
+		Intent intent = new Intent(this, ExploreMenuActivity.class);
+		//intent.putExtra(LOCATION_PHOTOS_DIR_PATH, locationPhotosDirPath);
+		startActivity(intent);
 	}
 }
